684302aa10b0129718001e10306990e519583290
Florens Pauwels <github@florens.be>
2022-04-10 21:56:30 +0200

    fabric-loom#611 test case

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,16 +1,19 @@
 package net.fabricmc.example.mixin;
 
 import net.fabricmc.example.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(TitleScreen.class)
+@Mixin(AnimalEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
+
+	// Note that interactMob is an overriding/overridden method, base method is on MobEntity
+	@Redirect(method = "interactMob", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/AnimalEntity;isBreedingItem(Lnet/minecraft/item/ItemStack;)Z"))
+	private boolean test(AnimalEntity instance, ItemStack stack) {
 		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		return true;
 	}
 }
